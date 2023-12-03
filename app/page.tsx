fd99a01c3a94066d208e333086f9a5e0b08f7b9f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-03 19:24:01 +0100

    Feat: Test elements

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import { Text } from "./components/elements";
+
 export default function Home() {
-  return (<div>Welcome to home page</div>)
+  return (<div>
+    <Text size='xl'>Belsac</Text>
+    <Text size='lg'>Belsac</Text>
+    <Text size='md'>Belsac</Text>
+    <Text size='sm'>Belsac</Text>
+    <Text size='xs'>Belsac</Text>
+    <p>Belsac</p>
+  </div>)
 }
