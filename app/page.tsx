5de3f6a679471f5734bae4bf225c611d677f5cfe
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-03 19:44:00 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,24 @@
 'use client'
 
-import { Button, Text } from "./components/elements";
+import { Button, Text } from './components/elements'
 
 export default function Home() {
-  return (<div>
-    <Text size='xl'>Belsac</Text>
-    <Text size='lg'>Belsac</Text>
-    <Text size='md'>Belsac</Text>
-    <Text size='sm'>Belsac</Text>
-    <Text size='xs'>Belsac</Text>
-    <Button onClick={() => {console.log('clicked')}}>
-      <Text size='sm' white>Click me</Text>
-    </Button>
-  </div>)
+  return (
+    <div>
+      <Text size='xl'>Belsac</Text>
+      <Text size='lg'>Belsac</Text>
+      <Text size='md'>Belsac</Text>
+      <Text size='sm'>Belsac</Text>
+      <Text size='xs'>Belsac</Text>
+      <Button
+        onClick={() => {
+          console.log('clicked')
+        }}
+      >
+        <Text size='sm' white>
+          Click me
+        </Text>
+      </Button>
+    </div>
+  )
 }
