bf946627f474ec4aa797001ac17819445e603bb1
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-03 18:52:47 +0100

    Fix: Format with prettier

--- app/components/elements.tsx
+++ app/components/elements.tsx
@@ -1,12 +1,18 @@
-export function Text({faded, copy, size, children}: {faded?: boolean, copy?: boolean, size: 'xl' | 'lg' | 'md' | 'sm' | 'xs', children: string}) {
-  const sizePair = {xl: '4xl', lg: '3xl', md: '2xl', sm: 'xl', xs: 'lg'}
+export function Text({
+  faded,
+  copy,
+  size,
+  children,
+}: {
+  faded?: boolean
+  copy?: boolean
+  size: 'xl' | 'lg' | 'md' | 'sm' | 'xs'
+  children: string
+}) {
+  const sizePair = { xl: '4xl', lg: '3xl', md: '2xl', sm: 'xl', xs: 'lg' }
   const sizeStyle = `text-${sizePair[size]} font-light`
   const isCopy = `${copy ? 'leading-snug' : 'leading-none'}`
   const isFaded = `${faded ? 'text-black/65' : 'text-black'}`
 
-  return (
-    <p className={`${sizeStyle} ${isCopy} ${isFaded}`}>
-      {children}
-    </p>
-  )
+  return <p className={`${sizeStyle} ${isCopy} ${isFaded}`}>{children}</p>
 }
