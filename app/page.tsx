031f39b2d53bd54a6e0c18cd68b562684e3c2e64
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-04 06:33:46 +0100

    Fix: Format with prettier

--- app/page.tsx
+++ app/page.tsx
@@ -20,13 +20,65 @@ export default function Home() {
           Click me
         </Text>
       </Button>
-      <TextLink href='/' size='sm'>A Product Name</TextLink>
+      <TextLink href='/' size='sm'>
+        A Product Name
+      </TextLink>
       <div className='flex justify-start items-center gap-10'>
-      <Card product={{title: 'A Product Name For A Beautiful Bag', handle: 'a-product-name-for-a-beautiful-bag', featuredImage: 'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0', price: '$100', compareAtPrice: '$200', collectionHandle: 'collection-handle'}} />
-      <Card product={{title: 'A Product Name For A Beautiful Bag', handle: 'a-product-name-for-a-beautiful-bag', featuredImage: 'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0', price: '$100', compareAtPrice: '$200', collectionHandle: 'collection-handle'}} />
-      <Card product={{title: 'A Product Name For A Beautiful Bag', handle: 'a-product-name-for-a-beautiful-bag', featuredImage: 'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0', price: '$100', compareAtPrice: '$200', collectionHandle: 'collection-handle'}} />
-      <Card product={{title: 'A Product Name For A Beautiful Bag', handle: 'a-product-name-for-a-beautiful-bag', featuredImage: 'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0', price: '$100', compareAtPrice: '$200', collectionHandle: 'collection-handle'}} />
-      <Card product={{title: 'A Product Name For A Beautiful Bag', handle: 'a-product-name-for-a-beautiful-bag', featuredImage: 'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0', price: '$100', compareAtPrice: '$200', collectionHandle: 'collection-handle'}} />
+        <Card
+          product={{
+            title: 'A Product Name For A Beautiful Bag',
+            handle: 'a-product-name-for-a-beautiful-bag',
+            featuredImage:
+              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
+            price: '$100',
+            compareAtPrice: '$200',
+            collectionHandle: 'collection-handle',
+          }}
+        />
+        <Card
+          product={{
+            title: 'A Product Name For A Beautiful Bag',
+            handle: 'a-product-name-for-a-beautiful-bag',
+            featuredImage:
+              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
+            price: '$100',
+            compareAtPrice: '$200',
+            collectionHandle: 'collection-handle',
+          }}
+        />
+        <Card
+          product={{
+            title: 'A Product Name For A Beautiful Bag',
+            handle: 'a-product-name-for-a-beautiful-bag',
+            featuredImage:
+              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
+            price: '$100',
+            compareAtPrice: '$200',
+            collectionHandle: 'collection-handle',
+          }}
+        />
+        <Card
+          product={{
+            title: 'A Product Name For A Beautiful Bag',
+            handle: 'a-product-name-for-a-beautiful-bag',
+            featuredImage:
+              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
+            price: '$100',
+            compareAtPrice: '$200',
+            collectionHandle: 'collection-handle',
+          }}
+        />
+        <Card
+          product={{
+            title: 'A Product Name For A Beautiful Bag',
+            handle: 'a-product-name-for-a-beautiful-bag',
+            featuredImage:
+              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
+            price: '$100',
+            compareAtPrice: '$200',
+            collectionHandle: 'collection-handle',
+          }}
+        />
       </div>
     </div>
   )
