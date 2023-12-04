16c265b3ae7ad91281eebd85418a8e4b741f2ed5
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-04 06:37:34 +0100

    Fix: Inprove on the style

--- app/components/product/card.tsx
+++ app/components/product/card.tsx
@@ -23,7 +23,7 @@ export default function Card({ product }: Props) {
   } = product
 
   return (
-    <div className='shadow-lg w-1/5 border border-black/65 flex flex-col gap-0 '>
+    <div className='shadow-lg w-1/5 border border-black/65 flex flex-col gap-0 group'>
       <Image
         src={featuredImage}
         alt={title}
