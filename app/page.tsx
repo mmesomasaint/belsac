b3864d47c4f040b8140f86798f1726d4a456a09f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-04 07:16:46 +0100

    Feat: Redesign home pg

--- app/page.tsx
+++ app/page.tsx
@@ -1,84 +1,22 @@
 'use client'
 
+import { PiShoppingCartThin } from "react-icons/pi"
 import { Button, Text, TextLink } from './components/elements'
 import Card from './components/product/card'
 
 export default function Home() {
   return (
     <div>
-      <Text size='xl'>Belsac</Text>
-      <Text size='lg'>Belsac</Text>
-      <Text size='md'>Belsac</Text>
-      <Text size='sm'>Belsac</Text>
-      <Text size='xs'>Belsac</Text>
-      <Button
-        onClick={() => {
-          console.log('clicked')
-        }}
-      >
-        <Text size='sm' white>
-          Click me
-        </Text>
-      </Button>
-      <TextLink href='/' size='sm'>
-        A Product Name
-      </TextLink>
-      <div className='flex justify-start items-center gap-10'>
-        <Card
-          product={{
-            title: 'A Product Name For A Beautiful Bag',
-            handle: 'a-product-name-for-a-beautiful-bag',
-            featuredImage:
-              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
-            price: '$100',
-            compareAtPrice: '$200',
-            collectionHandle: 'collection-handle',
-          }}
-        />
-        <Card
-          product={{
-            title: 'A Product Name For A Beautiful Bag',
-            handle: 'a-product-name-for-a-beautiful-bag',
-            featuredImage:
-              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
-            price: '$100',
-            compareAtPrice: '$200',
-            collectionHandle: 'collection-handle',
-          }}
-        />
-        <Card
-          product={{
-            title: 'A Product Name For A Beautiful Bag',
-            handle: 'a-product-name-for-a-beautiful-bag',
-            featuredImage:
-              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
-            price: '$100',
-            compareAtPrice: '$200',
-            collectionHandle: 'collection-handle',
-          }}
-        />
-        <Card
-          product={{
-            title: 'A Product Name For A Beautiful Bag',
-            handle: 'a-product-name-for-a-beautiful-bag',
-            featuredImage:
-              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
-            price: '$100',
-            compareAtPrice: '$200',
-            collectionHandle: 'collection-handle',
-          }}
-        />
-        <Card
-          product={{
-            title: 'A Product Name For A Beautiful Bag',
-            handle: 'a-product-name-for-a-beautiful-bag',
-            featuredImage:
-              'https://images.selfridges.com/is/image/selfridges/R04229467_SAND_M?wid=476&hei=634&fmt=webp&qlt=80,1&bgc=F6F6F6&extend=-18,0,-18,0',
-            price: '$100',
-            compareAtPrice: '$200',
-            collectionHandle: 'collection-handle',
-          }}
-        />
+      <div className='flex justify-between items-center gap-10'>
+        <div className=''>
+          <Text size='xl'>Belsac</Text>
+        </div>
+        <div className=''>
+          <Button onClick={() => console.log('Search button clicked')}>Search</Button>
+        </div>
+        <div className='flex justify-end items-center gap-4'>
+        <PiShoppingCartThin className='text-3xl' />
+        </div>
       </div>
     </div>
   )
