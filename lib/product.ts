e87e6a0b37c0bbd922ca0393837c31eb255b5a0b
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-07 06:40:25 +0100

    Feat: Create the product lib file

---
+++ lib/product.ts
@@ -0,0 +1,10 @@
+export type MiniProduct = {
+  product: {
+    title: string
+    handle: string
+    featuredImage: string
+    price: string
+    compareAtPrice: string
+    collectionHandle: string
+  }
+}
