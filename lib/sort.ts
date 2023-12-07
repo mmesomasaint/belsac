37d94a8912a9328aa356f81d75395f570414b8e5
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-07 07:22:45 +0100

    Fix: Write the sorting functions

--- lib/sort.ts
+++ lib/sort.ts
@@ -1,40 +1,42 @@
+import { MiniProduct } from "./product";
+
 // Sort ascending
-function sortByNameAsc(products) {
+function sortByNameAsc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return a.name.localeCompare(b.name);
   });
 }
 
 // Sort descending
-function sortByNameDesc(products) {
+function sortByNameDesc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return b.name.localeCompare(a.name);
   });
 }
 
 // Sort ascending
-function sortByDateAsc(products) {
+function sortByDateAsc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return a.date - b.date;
   });
 }
 
 // Sort descending
-function sortByDateDesc(products) {
+function sortByDateDesc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return b.date - a.date;
   });
 }
 
 // Sort ascending
-function sortByPriceAsc(products) {
+function sortByPriceAsc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return a.price - b.price;
   });
 }
 
 // Sort descending
-function sortByPriceDesc(products) {
+function sortByPriceDesc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return b.price - a.price;
   });
