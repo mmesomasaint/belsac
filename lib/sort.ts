f5883304da9e2e811d54d628464d2553ffbd8c21
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-09 06:39:52 +0100

    Feat: Document sort fxns

--- lib/sort.ts
+++ lib/sort.ts
@@ -1,41 +1,71 @@
 import { MiniProduct } from "./product";
 
-// Sort ascending
+/**
+ * Sorts a list of products objects based on the title field,
+ * in ascending alphabetical order.
+ * @param products The list of products to sort.
+ * @returns The sorted list of products.
+ */
 function sortByNameAsc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return a.name.localeCompare(b.name);
   });
 }
 
-// Sort descending
+/**
+ * Sorts a list of products objects based on the title field,
+ * in descending alphabetical order.
+ * @param products The list of products to sort.
+ * @returns The sorted list of products.
+ */
 function sortByNameDesc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return b.name.localeCompare(a.name);
   });
 }
 
-// Sort ascending
+/**
+ * Sorts a list of products objects based on the date field,
+ * in ascending order.
+ * @param products The list of products to sort.
+ * @returns The sorted list of products.
+ */
 function sortByDateAsc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return a.date - b.date;
   });
 }
 
-// Sort descending
+/**
+ * Sorts a list of products objects based on the date field,
+ * in descending order.
+ * @param products The list of products to sort.
+ * @returns The sorted list of products.
+ */
 function sortByDateDesc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return b.date - a.date;
   });
 }
 
-// Sort ascending
+/**
+ * Sorts a list of products objects based on the price field,
+ * in ascending order.
+ * @param products The list of products to sort.
+ * @returns The sorted list of products.
+ */
 function sortByPriceAsc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return a.price - b.price;
   });
 }
 
-// Sort descending
+/**
+ * Sorts a list of products objects based on the price field,
+ * in descending order.
+ * @param products The list of products to sort.
+ * @returns The sorted list of products.
+ */
 function sortByPriceDesc(products: MiniProduct[]) {
   return products.slice().sort((a, b) => {
     return b.price - a.price;
