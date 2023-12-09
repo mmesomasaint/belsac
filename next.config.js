b878c02af6cc9ef8e3ca1284ed39a5bbfec81f13
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-09 08:33:02 +0100

    Fix: Use shopify hostname for img

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.selfridges.com',
+        hostname: 'cdn.shopify.com',
       },
     ],
   },
