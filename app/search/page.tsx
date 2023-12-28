b8b959ab3669ab95171828b0f082acb7823168e4
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-28 23:21:29 +0100

    Fix: Format with prettier

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -61,52 +61,55 @@ export default function Search() {
       <Header />
       <div className='py-4 mt-12'>
         <div className='grid grid-cols-4 place-content-between items-stretch gap-8'>
-          
-        <div className='flex flex-col gap-8 p-8 w-full bg-gray-300'>
-          <Text size='lg'>Filter</Text>
-          {filter && <FilterBar filter={filter} setFilter={setFilter} />}
-        </div>
-        <div className='col-span-3 w-full'>
-          
-        <div className='flex justify-between items-end gap-10 py-8'>
-          {loading ? (
-            <Text size='lg'>&middot;&middot;&middot;</Text>
-          ) : (
-            <Text size='lg'>
-              {`${total} result${total > 1 ? 's' : ''} for "${query ?? ''}"`}
-            </Text>
-          )}
-          <Sort setSort={setSort} />
-        </div>
-        <div className='grid grid-cols-3 place-content-between items-stretch gap-16'>
-          
-        {loading ? (
-            <div className='w-full col-span-full flex justify-center items-center'>
-              <Text size='md'>Loading...</Text>
-            </div>
-          ) : hasError ? (
-            <div className='w-full col-span-full flex justify-center items-center'>
-              <Text size='md'>Something went wrong.</Text>
-            </div>
-          ) : products.length < 1 ? (
-            <div className='w-full col-span-full flex justify-center items-center'>
-              <Text size='md'>No products found. Search something else.</Text>
-            </div>
-          ) : products.map((product) => (
-            <Card key={product.id} product={product} full />
-          ))}
-          <div className='col-span-full flex justify-center items-center gap-8'>
-            {hasMore && (
-              <Button onClick={() => load()} outline>
-                <span className='flex justify-center items-center gap-4'>
-                  More
-                  <PiCaretDownThin className='text-4xl' />
-                </span>
-              </Button>
-            )}
+          <div className='flex flex-col gap-8 p-8 w-full bg-gray-300'>
+            <Text size='lg'>Filter</Text>
+            {filter && <FilterBar filter={filter} setFilter={setFilter} />}
           </div>
-        </div>
-        </div>
+          <div className='col-span-3 w-full'>
+            <div className='flex justify-between items-end gap-10 py-8'>
+              {loading ? (
+                <Text size='lg'>&middot;&middot;&middot;</Text>
+              ) : (
+                <Text size='lg'>
+                  {`${total} result${total > 1 ? 's' : ''} for "${
+                    query ?? ''
+                  }"`}
+                </Text>
+              )}
+              <Sort setSort={setSort} />
+            </div>
+            <div className='grid grid-cols-3 place-content-between items-stretch gap-16'>
+              {loading ? (
+                <div className='w-full col-span-full flex justify-center items-center'>
+                  <Text size='md'>Loading...</Text>
+                </div>
+              ) : hasError ? (
+                <div className='w-full col-span-full flex justify-center items-center'>
+                  <Text size='md'>Something went wrong.</Text>
+                </div>
+              ) : products.length < 1 ? (
+                <div className='w-full col-span-full flex justify-center items-center'>
+                  <Text size='md'>
+                    No products found. Search something else.
+                  </Text>
+                </div>
+              ) : (
+                products.map((product) => (
+                  <Card key={product.id} product={product} full />
+                ))
+              )}
+              <div className='col-span-full flex justify-center items-center gap-8'>
+                {hasMore && (
+                  <Button onClick={() => load()} outline>
+                    <span className='flex justify-center items-center gap-4'>
+                      More
+                      <PiCaretDownThin className='text-4xl' />
+                    </span>
+                  </Button>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
