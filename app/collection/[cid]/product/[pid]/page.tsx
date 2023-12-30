aab1748d78f0c1f5a623392b820b6b822ede1f87
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-30 21:52:06 +0100

    Feat: Create the product page

---
+++ app/collection/[cid]/product/[pid]/page.tsx
@@ -0,0 +1,17 @@
+import Header from "@/app/components/header"
+
+export default async function Product({params}: {params: {cid: string, pid: string}}) {
+  const {cid, pid} = params
+  const res = await fetch(`/api/products/product?handle=${pid}`)
+  const product = await res.json()
+  return (
+    <div className='px-7 max-w-[120rem] mx-auto'>
+      <Header />
+      <div className='py-4 mt-12'>
+        <div className='grid grid-cols-2 place-content-between items-stretch gap-16'>
+          Main content
+        </div>
+      </div>
+    </div>
+  )
+}
