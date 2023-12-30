02f0859c82d45332742fcafceae7d164f35af15d
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-30 22:55:22 +0100

    Fix: Build server component

--- app/collection/[cid]/product/[pid]/page.tsx
+++ app/collection/[cid]/product/[pid]/page.tsx
@@ -1,4 +1,27 @@
+import { RETRIEVE_A_PRODUCT } from '@/app/api/query';
+import { cleanProduct } from '@/app/api/utils';
+import { Text } from '@/app/components/elements';
 import Header from '@/app/components/header'
+import ProductSlider from '@/app/components/product/slider';
+import { shopifyFetch } from '@/lib/fetch';
+import { formatMoney } from '@/lib/product';
+import { PiCaretRightThin } from 'react-icons/pi';
+
+
+const fetchProduct = async (handle: string) => {
+  const variables = {
+    handle,
+  }
+
+  const {status, body} = await shopifyFetch({
+    query: RETRIEVE_A_PRODUCT,
+    variables,
+  })
+
+  if (status === 200) {
+    return cleanProduct(body.data?.product)
+  }
+}
 
 export default async function Product({
   params,
@@ -6,15 +29,27 @@ export default async function Product({
   params: { cid: string; pid: string }
 }) {
   const { cid, pid } = params
-  const res = await fetch(`/api/products/product?handle=${pid}`)
-  const product = await res.json()
-  
+  const product = await fetchProduct(pid)
+
   return (
     <div className='px-7 max-w-[120rem] mx-auto'>
       <Header />
       <div className='py-4 mt-12'>
+        <div className='flex justify-start items-center gap-5'>
+          <Text size='xs' faded>Home</Text>
+          <PiCaretRightThin className='text-2xl' />
+          <Text size='xs' faded>{cid.split('-').join(' ')}</Text>
+          <PiCaretRightThin className='text-2xl' />
+          <Text size='xs' faded>{product?.title ?? ''}</Text>
+        </div>
         <div className='grid grid-cols-2 place-content-between items-stretch gap-16'>
-          Main content
+          <div className='col-span-1'>
+            <ProductSlider images={product?.images ?? []} />
+          </div>
+          <div className='col-span-1'>
+            <Text size='xl'>{product?.title ?? '...'}</Text>
+            <Text size='sm'>{formatMoney(product?.price ?? 0)}</Text>
+          </div>
         </div>
       </div>
     </div>
