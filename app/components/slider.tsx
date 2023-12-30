166c9209558a24beff7d3317b288403a2a86b87f
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-30 21:53:44 +0100

    Feat: Create the product slider

---
+++ app/components/slider.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+interface ProductImage {
+  src: string;
+  alt: string;
+}
+
+const ProductSlider: React.FC<{ images: ProductImage[] }> = ({ images }) => {
+  const [activeImageIndex, setActiveImageIndex] = useState(0);
+
+  const handleImageClick = (index: number) => {
+    setActiveImageIndex(index);
+  };
+
+  return (
+    <div className="container mx-auto px-4">
+      <div className="relative">
+        <img
+          src={images[activeImageIndex].src}
+          alt={images[activeImageIndex].alt}
+          className="w-full h-40 object-cover rounded-lg"
+        />
+      </div>
+      <div className="mt-4">
+        <div className="flex overflow-x-auto">
+          {images.map((image, index) => (
+            <button
+              key={index}
+              type="button"
+              className={`relative w-1/5 mx-1 rounded-lg overflow-hidden cursor-pointer ${
+                activeImageIndex === index ? 'border-2 border-blue-500' : ''
+              }`}
+              onClick={() => handleImageClick(index)}
+            >
+              <img
+                src={image.src}
+                alt={image.alt}
+                className="w-full h-32 object-cover"
+              />
+            </button>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductSlider;
