da14701cb8211c6cc8035b8ccb18dc244e490dfd
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-30 22:55:31 +0100

    Fix: Format with prettier

--- app/collection/[cid]/product/[pid]/page.tsx
+++ app/collection/[cid]/product/[pid]/page.tsx
@@ -1,19 +1,18 @@
-import { RETRIEVE_A_PRODUCT } from '@/app/api/query';
-import { cleanProduct } from '@/app/api/utils';
-import { Text } from '@/app/components/elements';
+import { RETRIEVE_A_PRODUCT } from '@/app/api/query'
+import { cleanProduct } from '@/app/api/utils'
+import { Text } from '@/app/components/elements'
 import Header from '@/app/components/header'
-import ProductSlider from '@/app/components/product/slider';
-import { shopifyFetch } from '@/lib/fetch';
-import { formatMoney } from '@/lib/product';
-import { PiCaretRightThin } from 'react-icons/pi';
-
+import ProductSlider from '@/app/components/product/slider'
+import { shopifyFetch } from '@/lib/fetch'
+import { formatMoney } from '@/lib/product'
+import { PiCaretRightThin } from 'react-icons/pi'
 
 const fetchProduct = async (handle: string) => {
   const variables = {
     handle,
   }
 
-  const {status, body} = await shopifyFetch({
+  const { status, body } = await shopifyFetch({
     query: RETRIEVE_A_PRODUCT,
     variables,
   })
@@ -36,11 +35,17 @@ export default async function Product({
       <Header />
       <div className='py-4 mt-12'>
         <div className='flex justify-start items-center gap-5'>
-          <Text size='xs' faded>Home</Text>
+          <Text size='xs' faded>
+            Home
+          </Text>
           <PiCaretRightThin className='text-2xl' />
-          <Text size='xs' faded>{cid.split('-').join(' ')}</Text>
+          <Text size='xs' faded>
+            {cid.split('-').join(' ')}
+          </Text>
           <PiCaretRightThin className='text-2xl' />
-          <Text size='xs' faded>{product?.title ?? ''}</Text>
+          <Text size='xs' faded>
+            {product?.title ?? ''}
+          </Text>
         </div>
         <div className='grid grid-cols-2 place-content-between items-stretch gap-16'>
           <div className='col-span-1'>
