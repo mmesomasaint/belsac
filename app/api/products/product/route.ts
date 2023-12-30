7d374ace29b9bb8e93fd108b902e605d882bff86
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-30 21:21:36 +0100

    Feat: Create the product route api handler

---
+++ app/api/products/product/route.ts
@@ -0,0 +1,24 @@
+import { shopifyFetch } from '@/lib/fetch'
+import { NextRequest } from 'next/server'
+import { RETRIEVE_A_PRODUCT } from '../../query'
+import { cleanProduct } from '../../utils'
+
+export async function GET(Request: NextRequest) {
+  const searchParams = Request.nextUrl.searchParams
+  const handle = searchParams.get('handle')
+  const variables = {
+    handle,
+  }
+
+  const { status, body } = await shopifyFetch({
+    query: RETRIEVE_A_PRODUCT,
+    variables,
+  })
+
+  if (status === 200) {
+    const product = cleanProduct(body.data?.product)
+    return Response.json({ status: 200, body: product })
+  } else {
+    return Response.json({ status: 500, message: 'Error receiving data' })
+  }
+}
