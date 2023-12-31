ca49a63cc56d7a4983894271d8ddd57b1226a524
Mmesoma Saint <mmesomasaint@gmail.com>
2023-12-31 01:07:31 +0100

    Feat: Update the variant route

--- app/api/products/variant/route.ts
+++ app/api/products/variant/route.ts
@@ -1,7 +1,7 @@
 import { shopifyFetch } from '@/lib/fetch'
 import { NextRequest } from 'next/server'
-import { RETRIEVE_A_PRODUCT } from '../../query'
-import { cleanProduct } from '../../utils'
+import { GET_VARIANT_BY_SELECTED_OPTIONS } from '../../query'
+import { cleanProductVariant } from '../../utils'
 
 export async function GET(Request: NextRequest) {
   const { selectedOptions } = await Request.json()
@@ -13,12 +13,12 @@ export async function GET(Request: NextRequest) {
   }
 
   const { status, body } = await shopifyFetch({
-    query: RETRIEVE_A_PRODUCT,
+    query: GET_VARIANT_BY_SELECTED_OPTIONS,
     variables,
   })
 
   if (status === 200) {
-    const product = cleanProduct(body.data?.product)
+    const product = cleanProductVariant(body.data?.product)
     return Response.json({ status: 200, body: product })
   } else {
     return Response.json({ status: 500, message: 'Error receiving data' })
