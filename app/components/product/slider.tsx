865bb1598b4033076fb2210442ae36c73b324da4
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-01 02:29:46 +0100

    Feat: Update slider

--- app/components/product/slider.tsx
+++ app/components/product/slider.tsx
@@ -16,13 +16,13 @@ const ProductSlider: React.FC<{ images: ProductImage[] }> = ({ images }) => {
 
   return (
     <div className='container mx-auto flex max-h-[35rem]'>
-      <div className='max-h-full overflow-hidden'>
+      <div className='max-h-full overflow-y-hidden'>
         <div className='flex flex-col w-full h-full gap-4 overflow-y-auto'>
           {images.map((image, index) => (
             <button
               key={index}
               type='button'
-              className={`shrink-0 relative w-full h-1/5 mr-4 overflow-hidden cursor-pointer border-2 hover:border-black/40 ${
+              className={`shrink-0 relative w-full h-1/5 overflow-hidden cursor-pointer border-2 hover:border-black/40 ${
                 activeImageIndex === index
                   ? 'border-black'
                   : 'border-transparent'
