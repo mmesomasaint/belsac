f08ccf86261c1571a6bbf5b53ee7f698ad73040d
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-01 02:57:42 +0100

    Feat: Create the full-cart-query-result type

--- app/api/types.ts
+++ app/api/types.ts
@@ -113,3 +113,48 @@ export interface Merchandise {
     value: string
   }[]
 }
+
+export interface FullCartQueryResult {
+  id: string
+  lines: {
+    nodes: CartLine[]
+  }
+  attributes: {
+    key: string
+    value: string
+  }[]
+  cost: {
+    totalAmount: {
+      amount: number
+      currencyCode: string
+    }
+    subtotalAmount: {
+      amount: number
+      currencyCode: string
+    }
+    totalTaxAmount: {
+      amount: number
+      currencyCode: string
+    }
+    totalDutyAmount: {
+      amount: number
+      currencyCode: string
+    }
+  }
+  buyerIdentity: {
+    email: string
+    phone: string
+    customer: {
+      id: string
+    }
+    countryCode: string
+    deliveryAddressPreferences: {
+      address1: string
+      address2: string
+      city: string
+      provinceCode: string
+      countryCodeV2: string
+      zip: string
+    }
+  }
+}
--- app/api/utils.ts
+++ app/api/utils.ts
@@ -279,7 +279,7 @@ export function cleanMiniCartResult(queryResult: MiniCartQueryResult) {
 
   return {
     id,
-    cartLines
+    cartLines,
   }
 }
 
