e5a191c99947c31afbcfa1799892cc6c92c1bb8f
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-01 02:52:27 +0100

    Feat: Add retrieve-cart query

--- app/api/query.ts
+++ app/api/query.ts
@@ -213,3 +213,66 @@ query ($cartId: ID!) {
   }
 }
 `
+
+export const RETRIEVE_CART = `
+query ($cartId: ID!) {
+  cart(id: $cartId) {
+    id
+    lines(first: 10) {
+      nodes {
+        id
+        quantity
+        merchandise {
+          ... on ProductVariant {
+            id
+          }
+        }
+        attributes {
+          key
+          value
+        }
+      }
+    }
+    attributes {
+      key
+      value
+    }
+    cost {
+      totalAmount {
+        amount
+        currencyCode
+      }
+      subtotalAmount {
+        amount
+        currencyCode
+      }
+      totalTaxAmount {
+        amount
+        currencyCode
+      }
+      totalDutyAmount {
+        amount
+        currencyCode
+      }
+    }
+    buyerIdentity {
+      email
+      phone
+      customer {
+        id
+      }
+      countryCode
+      deliveryAddressPreferences {
+        ... on MailingAddress {
+          address1
+          address2
+          city
+          provinceCode
+          countryCodeV2
+          zip
+        }
+      }
+    }
+  }
+}
+`
