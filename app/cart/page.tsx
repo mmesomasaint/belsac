c064a7ea359ae9b716a8f8e30b8ed4f028d62b1b
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-01 02:51:50 +0100

    Feat: Create the cart page

---
+++ app/cart/page.tsx
@@ -0,0 +1,47 @@
+import { RETRIEVE_A_PRODUCT } from '@/app/api/query'
+import { cleanProduct } from '@/app/api/utils'
+import { Text } from '@/app/components/elements'
+import Header from '@/app/components/header'
+import ProductSlider from '@/app/components/product/slider'
+import { shopifyFetch } from '@/lib/fetch'
+import { PiCaretRightThin } from 'react-icons/pi'
+import DetailsPanel from './details'
+
+const getCart = async (cartId: string) => {
+  const variables = {
+    cartId,
+  }
+
+  const { status, body } = await shopifyFetch({
+    query: RETRIEVE_CART,
+    variables,
+  })
+
+  if (status === 200) {
+    return cleanFullCartResult(body.data?.cart)
+  }
+}
+
+export default async function Product() {
+  const cart = await getCart(cartId)
+
+  return (
+    <div className='px-7 max-w-[120rem] mx-auto'>
+      <Header />
+      <div className='py-4 mt-12'>
+        <div className='flex justify-start items-center gap-5'>
+          <Text size='xs' faded>
+            Home
+          </Text>
+          <PiCaretRightThin className='text-2xl' />
+          <Text size='xs' faded>
+            Cart
+          </Text>
+        </div>
+        <div className='grid grid-cols-2 place-content-between items-stretch gap-8 py-6'>
+          
+        </div>
+      </div>
+    </div>
+  )
+}
