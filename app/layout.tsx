3d2c5593cfd264a889186cf40c51bb838b6f4a31
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-01 02:31:44 +0100

    Feat: Update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
 import { CartProvider } from './hooks/usecart'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
