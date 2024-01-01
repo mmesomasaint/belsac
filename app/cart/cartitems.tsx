b97a6ea202686ba97dad6edfc99ce06ed668ea85
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-01 14:33:35 +0100

    Fix: Format with prettier

--- app/cart/cartitems.tsx
+++ app/cart/cartitems.tsx
@@ -2,16 +2,19 @@
 
 import { useEffect, useState } from 'react'
 import { TbShoppingCartOff } from 'react-icons/tb'
-import { PiPencilSimpleLineThin, PiTrashSimpleThin } from "react-icons/pi";
+import { PiPencilSimpleLineThin, PiTrashSimpleThin } from 'react-icons/pi'
 import useCart from '../hooks/usecart'
 import { Text } from '../components/elements'
 import Image from 'next/image'
 import { formatMoney } from '@/lib/product'
 
-interface Attribute {key: string, value: string}
+interface Attribute {
+  key: string
+  value: string
+}
 
 interface CartLine {
-  attributes: Attribute[] | {[key: string]: any}
+  attributes: Attribute[] | { [key: string]: any }
   id: string
   merchandiseId: string
   quantity: number
@@ -45,15 +48,17 @@ export default function CartItems() {
 
   const extractAttributes = (lines: CartLine[]) => {
     return lines.map((line) => {
-      const attributes = Object.fromEntries(line.attributes.map(({key, value}: Attribute) => [key, value]))
-      const {price, title, featuredImage, ...rest} = attributes
+      const attributes = Object.fromEntries(
+        line.attributes.map(({ key, value }: Attribute) => [key, value])
+      )
+      const { price, title, featuredImage, ...rest } = attributes
 
       return {
         title,
         featuredImage,
         price: Number(price),
         ...line,
-        attributes: rest
+        attributes: rest,
       }
     })
   }
@@ -73,7 +78,7 @@ export default function CartItems() {
           console.log('Cart Information: \n', data?.body)
           setCart({
             cartLines: extractAttributes(data?.body?.cartLines),
-            ...data?.body
+            ...data?.body,
           })
         })
         .finally(() => setLoading(false))
@@ -110,12 +115,22 @@ export default function CartItems() {
   )
 }
 
-function CartItem({ line: {id, title, featuredImage, price, quantity} }: { line: CartLine }) {
+function CartItem({
+  line: { id, title, featuredImage, price, quantity },
+}: {
+  line: CartLine
+}) {
   const hasImage = featuredImage && title
   return (
     <div className='relative grid grid-cols-6 place-items-stretch min-h-[20rem] gap-10 p-10 border-b border-black/40 last:border-none'>
       <div className='col-span-2 w-full bg-gray-300'>
-      {hasImage && <Image src={featuredImage} alt={title} className='w-full object-cover'/>}
+        {hasImage && (
+          <Image
+            src={featuredImage}
+            alt={title}
+            className='w-full object-cover'
+          />
+        )}
       </div>
       <div className='col-span-4 w-full flex flex-col gap-6'>
         <Text size='lg'>{title ?? '...'}</Text>
@@ -124,12 +139,22 @@ function CartItem({ line: {id, title, featuredImage, price, quantity} }: { line:
         </Text>
       </div>
       <div className='absolute top-0 right-0 flex items-stretch justify-center gap-4'>
-      <button type='button' onClick={() => console.log(`Line: ${id}, ready for editing!!`)} className='w-14 h-14 flex justify-center items-center border border-black hover:bg-black/10 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black' title='Edit line'>
-        <PiPencilSimpleLineThin className='text-2xl text-black' />
-      </button>
-      <button type='button' onClick={() => console.log(`Line: ${id}, deleted!!`)} className='w-14 h-14 flex justify-center items-center border border-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black' title='Delete line'>
-        <PiTrashSimpleThin className='text-2xl text-red-500' />
-      </button>
+        <button
+          type='button'
+          onClick={() => console.log(`Line: ${id}, ready for editing!!`)}
+          className='w-14 h-14 flex justify-center items-center border border-black hover:bg-black/10 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black'
+          title='Edit line'
+        >
+          <PiPencilSimpleLineThin className='text-2xl text-black' />
+        </button>
+        <button
+          type='button'
+          onClick={() => console.log(`Line: ${id}, deleted!!`)}
+          className='w-14 h-14 flex justify-center items-center border border-red-500 hover:bg-red-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-black'
+          title='Delete line'
+        >
+          <PiTrashSimpleThin className='text-2xl text-red-500' />
+        </button>
       </div>
     </div>
   )
