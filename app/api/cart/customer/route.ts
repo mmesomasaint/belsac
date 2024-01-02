c45e41991db055ad81f4d4ee2065d8a1a2fef959
Mmesoma Saint <mmesomasaint@gmail.com>
2024-01-02 13:28:51 +0100

    Feat: Create the customer API route

---
+++ app/api/cart/customer/route.ts
@@ -0,0 +1,22 @@
+import { shopifyFetch } from "@/lib/fetch"
+import { NextRequest } from "next/server"
+import { UPDATE_CUSTOMER_INFO } from "../../query"
+import { cleanCustomerInfoResult } from "../../utils"
+
+export async function POST(Request: NextRequest) {
+  const searchParams = Request.nextUrl.searchParams
+  const cartId = searchParams.get('cartId')
+  const { customerInfo } = await Request.json()
+
+  const { status, body } = await shopifyFetch({
+    query: UPDATE_CUSTOMER_INFO,
+    variables: { cartId, buyerIdentity: generateBuyerId(customerInfo) },
+  })
+
+  if (status === 200) {
+    const cart = cleanCustomerInfoResult(body.data?.cartBuyerIdentityUpdate?.cart)
+    return Response.json({ status: 200, body: cart })
+  } else {
+    return Response.json({ status: 500, message: 'Error receiving data' })
+  }
+}
